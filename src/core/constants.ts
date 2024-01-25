7fca656e79841002ee1bc292f7ae332411a0cd96
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-01-25 13:08:11 +0100

    fix

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,2 +1,2 @@
 export const isDev = import.meta.env.DEV;
-export const BASE_URL = isDev ? 'https://cloud.sensearena.com/crm/dev' : 'https://cloud.sensearena.com/crm/release';
+export const BASE_URL = 'https://cloud.sensearena.com/crm/dev';
